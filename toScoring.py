1a023476a7dba347dda976b82ba7879d381db835
rafaelcespino <55030277+rafaelcespino@users.noreply.github.com>
2021-03-19 16:15:56 -0500

    Add files via upload

--- toScoring.py
+++ toScoring.py
@@ -10,21 +10,55 @@ for file in os.listdir(inDir):
     fileName = os.path.splitext(file)[0]
     print("Converting {}".format(fileName))
     resMat = sio.loadmat(inputMat)
-    result = resMat['result']
-    result = np.array(result)
-    result = result[0]
+    results = resMat['result']
+    results = np.array(results)
+    results = results[0]
 
     #lists of start and end times for speech and silence moments 
-    speechStarts = []
-    speechEnds = []
-    silenceStarts = []
-    silenceEnds = []
-    index = 0
-
-    arraySize = result.size
-    output = open('./{}.txt'.format(fileName), 'w')
 
 
+    arraySize = results.size
+    output = open('./txtLabels/{}.txt'.format(fileName), 'w')
+    nonSpeechLine = "X\tX\tX\tSAD\tX\t{}\t{}\tnon-speech\t0.500000\n"
+    speechLine = "X\tX\tX\tSAD\tX\t{}\t{}\tspeech\t0.500000\n"
+
+    if(results[0] == 0):
+        isSilence = True
+        lastSilenceStart = 0
+        for index, result in enumerate(results):
+
+            #switching from silence to speech 
+            if isSilence and result == 1:
+                isSilence = False
+                silenceEnd = round(((index - 1)/8000), 2)
+                output.write(nonSpeechLine.format(lastSilenceStart, silenceEnd))
+                lastSpeechStart = round(((index-1)/8000),2)
+                isSilence = False
+
+            #switching from speech to silence 
+            if not isSilence and result == 0:
+                isSilence = True
+                speechEnd = round(((index - 1)/8000), 2)
+                output.write(speechLine.format(lastSpeechStart, speechEnd))
+                lastSilenceStart = round(((index-1)/8000),2)
+                isSilence = True
+
+            #if silence until the end of the file
+            if isSilence and index == arraySize-1:
+                silenceEnd = round(((index - 1)/8000), 2)
+                output.write(nonSpeechLine.format(lastSilenceStart, silenceEnd))
+                break
+
+            #if speech until the end of the file 
+            if not isSilence and index == arraySize-1:
+                speechEnd = round(((index - 1)/8000), 2)
+                output.write(speechLine.format(lastSpeechStart, speechEnd))
+                break
+
+
+
+
+'''
     #if the array starts with silence
     if(result[0] == 0):
         print("Starts in silence")
@@ -61,3 +95,4 @@ for file in os.listdir(inDir):
                 elif forIndex == numSilenceStarts-1:
                     output.write("X\tX\tX\tSAD\tX\t{}\t{}\tnon-speech\t0.500000".format(silenceStart, round((arraySize/8000),2)))
                 forIndex += 1
+                '''
